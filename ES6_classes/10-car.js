c66fcb8a4493bec929f80f61723015cc5cfb6cef
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-03-28 12:52:50 -0400

    Vroom

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,15 +1,15 @@
 class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    // Método para clonar el coche
-    cloneCar() {
-        const ClonedCar = this.constructor[Symbol.species] || this.constructor;
-        return new ClonedCar();
-    }
+  // Método para clonar el coche
+  cloneCar() {
+    const ClonedCar = this.constructor[Symbol.species] || this.constructor;
+    return new ClonedCar();
+  }
 }
 
-export default Car; 
+export default Car;
