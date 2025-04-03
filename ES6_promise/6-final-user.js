324d662b45b321953916e9f74cfcfc45b6ed8313
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-04-03 14:12:17 -0400

    task 6: Handle multiple promises using Promise.all

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((results) => ({
+      status: 'fulfilled',
+      value: results,
+    }))
+    .catch(() => ({
+      status: 'rejected',
+      value: 'Error: Cannot load user',
+    }));
+}
