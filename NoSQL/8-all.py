e350fc7770e12ea1e61d51db8e6ea2c10278e4c1
Adolfo <adolfo.rodriguez2231@gmail.com>
2025-05-03 14:12:25 -0400

    Add Python function list_all with type hints that returns all documents from a MongoDB collection

---
+++ NoSQL/8-all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for listing all documents in a collection
+"""
+from typing import List, Dict, Any
+from pymongo.collection import Collection
+
+
+def list_all(mongo_collection: Collection) -> List[Dict[str, Any]]:
+    """
+    Lists all documents in a collection
+    Args:
+        mongo_collection: pymongo collection object
+    Returns:
+        List of all documents or empty list if no documents
+    """
+    documents = []
+    for doc in mongo_collection.find():
+        documents.append(doc)
+    return documents
