e0c4f62858e3da6c5970f9ad6f09b7cd42eae1f0
Adolfo <adolfo.rodriguez2231@gmail.com>
2025-05-19 13:06:29 -0400

    fix new line

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,20 +4,20 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
     countStudents(process.argv[2])
       .then((output) => {
-        res.end(output + '\n');
+        res.end(output);
       })
       .catch((error) => {
-        res.end(error.message + '\n');
+        res.end(error.message);
       });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
+    res.end('Not Found');
   }
 });
 
